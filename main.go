ab049b4fd24a0ea3869bc53926b8cd5d099e1491
Meltens <irimamekun@gmail.com>
2018-05-30 11:06:30 +0900

    Added: func random

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 // Package main provides ...
 package main
 
-import ()
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
+	n := int(random(1, 100))
+	fmt.Println(n)
+}
+
+func random(min, max float64) float64 {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Float64()*(max-min) + min
 }
